75703fa2e4727ac75b8f4f863b113c65e57f559e
Prakash A <prakashaathi321@gmail.com>
2022-10-11 18:15:35 +0530

    upgraded inc

--- assets/main.js
+++ assets/main.js
@@ -2,13 +2,26 @@ let recipeName = document.getElementById("recipename")
 let recipebtn  = document.getElementById("recipebtn")
 let title = document.getElementById("title")
 let recipe = document.getElementById("recipe")
-// console.log(recipe);
-
+let table = document.getElementById('table');
+let incBtn = document.getElementById("inc-btn");
+let incName = document.getElementById("inc-name")
+let incGram = document.getElementById("inc-gram")
 
 recipebtn.addEventListener("click", () => {
         // console.log(recipeName.value)
         title.innerHTML=recipeName.value.toUpperCase()+" INCREDIENTS";
 });
+
+incBtn.addEventListener('click',() => {
+    let row = document.createElement("tr")
+    let data1 = document.createElement("td")
+    let data2 = document.createElement("td")
+    data1.innerHTML= incName.value;
+    data2.innerHTML= incGram.value;
+    row.append(data1,data2)
+    console.log(row);
+    table.append(row);
+})
 
 function removeph(event){
     // let recipe = document.getElementById("recipe")
